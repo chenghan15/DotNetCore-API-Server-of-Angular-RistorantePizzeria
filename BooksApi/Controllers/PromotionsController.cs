3e8c5018056b34967041e3be9c7d59fd23b6b8bd
chenghan15 <hanwfifteen@gmail.com>
2019-08-29 23:49:36 +1200

    add Promotion controller

---
+++ BooksApi/Controllers/PromotionsController.cs
@@ -0,0 +1,74 @@
+﻿using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BooksApi.Controllers
+{
+    [Route("promotion/[controller]")]
+    [ApiController]
+    public class PromotionsController : ControllerBase
+    {
+        private readonly PromotionService _promotionService;
+
+        public PromotionsController(PromotionService promotionService)
+        {
+            _promotionService = promotionService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Promotion>> Get() =>
+            _promotionService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetPromotion")]
+        public ActionResult<Promotion> Get(string id)
+        {
+            var promotion = _promotionService.Get(id);
+
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+
+            return promotion;
+        }
+
+        [HttpPost]
+        public ActionResult<Promotion> Create(Promotion promotion)
+        {
+            _promotionService.Create(promotion);
+
+            return CreatedAtRoute("GetPromotion", new { id = promotion.Id.ToString() }, promotion);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Promotion promotionIn)
+        {
+            var promotion = _promotionService.Get(id);
+
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+
+            _promotionService.Update(id, promotionIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var promotion = _promotionService.Get(id);
+
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+
+            _promotionService.Remove(promotion.Id);
+
+            return NoContent();
+        }
+    }
+}
